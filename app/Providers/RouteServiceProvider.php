aa84dde9b8463bc5a8b159a163fd11a81a249f86
Sadegh Barzegar <sadegh19b@gmail.com>
2022-10-04 16:48:27 +0330

    Add configs and translations in route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,6 +38,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
         });
 
+        $this->configs();
+        $this->translations();
         $this->ziggy();
     }
 
@@ -46,14 +48,55 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function configureRateLimiting()
+    protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
 
-    public function ziggy(): void
+    protected function configs(): void
+    {
+        Route::get('js/configs.js', function () {
+            $configs['lang'] = config('app.locale');
+            $configs['rtl'] = config('app.rtl');
+            $configs['version'] = config('app.version');
+            $configs['name'] = config('app.name');
+
+            return response('window._app = '.json_encode($configs).';')
+                ->header('Content-Type', 'text/javascript');
+        })->name('configs.js');
+    }
+
+    protected function translations(): void
+    {
+        Route::get('js/translations.js', function () {
+            /*$strings = \Cache::rememberForever("lang_{$lang}.js", function () use ($lang) {
+                $strings = [];
+                foreach (glob(lang_path($lang).DIRECTORY_SEPARATOR.'*.php') as $file) {
+                    $strings[basename($file, '.php')] = require $file;
+                }
+                return $strings;
+            });*/
+
+            $lang = config('app.locale');
+            $strings = [];
+
+            foreach (glob(lang_path($lang).DIRECTORY_SEPARATOR.'*.php') as $file) {
+                $strings[basename($file, '.php')] = require $file;
+            }
+
+            if (file_exists(lang_path("{$lang}.json"))) {
+                $json = \File::get(lang_path("{$lang}.json"));
+                $strings['json'] = json_decode($json, true);
+            }
+
+            return response('window.i18n = '.json_encode($strings).';')
+                ->header('Content-Type', 'text/javascript');
+        })->name('translations.js');
+    }
+
+    protected function ziggy(): void
     {
         Route::get('js/ziggy.js', function () {
             return response(
@@ -62,7 +105,7 @@ class RouteServiceProvider extends ServiceProvider
                     '',
                     app(BladeRouteGenerator::class)->generate()
                 ))
-            )->header('Content-Type', 'application/javascript');
+            )->header('Content-Type', 'text/javascript');
         })->name('ziggy.js');
     }
 }
